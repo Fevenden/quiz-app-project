87daa34b3151e5baafa9a7d935ae71e554c55a77
Freedom Evenden <ftevenden1@gmail.com>
2019-11-24 20:32:20 -0900

    moved all images into a single file and modified their name in corresponding html files, created a store array with all quiz questions as opjects ,

---
+++ store.js
@@ -0,0 +1,57 @@
+const STORE = [
+  //question 1
+  {
+    question: 'What is the Dragonborn?',
+    options: [
+      'A small baby dragon',
+      'A mortal with the soul of a Dova',
+      'Someone who has far to much time on their hands',
+      'The Imperial Emperor'
+    ],
+    answer: 'A mortal with the soul of a Dova'
+  },
+  //question 2
+  {
+    question: 'Who are the Greybeards?',
+    options: [
+      'Masters of the way of the voice.',
+      'Crazy old men who scream at the sky.',
+      'Men who have cultivated the ultimate facial hair',
+      'Some guys in need of a shower.'
+    ],
+    answer: 'Masters of the way of the voice',
+  },
+  //question 3
+  {
+    question: 'What does Lydia do?',
+    options: [
+      'Block of doorways.',
+      'Begrudgingly carries your burdens.',
+      'Get fus ro dahed off cliffs.',
+      'All of the above',
+    ],
+    answer: 'All of the above',
+  },
+  //question 4
+  { 
+    question: 'what does fus ro dah mean?',
+    options: [
+      'Focus on that.',
+      'Fussy running dog.',
+      'Force balance push.',
+      'Force crush destroy.',
+    ],
+    answer: 'Force balance push.',
+  },
+  //question 5
+  {
+    question: 'what will all denizens skyrim die to defend?'
+    options: [
+      'Honor.',
+      'Decency',
+      'Goodness',
+      'A chicken',
+    ],
+    answer: 'A chicken',
+  }
+];
